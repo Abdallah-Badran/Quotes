dd13ea38914de541e9e96fa7fbf65c5fc5605ce8
Abdallah-Badran <abdallah.amr.badran@gmail.com>
2022-11-19 16:53:51 +0200

    Prepare client for build

--- client/src/api/quotes.js
+++ client/src/api/quotes.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 import { getToken } from '../browserStorage/auth'
 
 export const getQuote = async ()=>{
-    const res = await axios.get('http://localhost:5000/quote/random', {headers: {'Authorization':getToken()}})
+    const res = await axios.get('/quote/random', {headers: {'Authorization':getToken()}})
     return res
 }
