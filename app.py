d4ccddb34d6401163735b11ee901148107c1a82b
Abdallah-Badran <abdallah.amr.badran@gmail.com>
2022-11-19 17:01:24 +0200

    Consume built client

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.quotes import quotes_routes
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.register_blueprint(quotes_routes)
+
+
+@app.get('/')
+def home():
+    return render_template('./index.html')
+
