5e70df1fd2634b5fd5455236875f6cf218bbf66e
Abdallah-Badran <abdallah.amr.badran@gmail.com>
2022-11-20 08:36:49 +0200

    Disable cors

--- routes/quotes.py
+++ routes/quotes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 from views import QuoteView, auth
-from flask_cors import cross_origin
+# from flask_cors import cross_origin
 
 quotes_routes = Blueprint("quotes_routes", __name__)
 
 
 @quotes_routes.get('/quote/random')
-@cross_origin()
+# @cross_origin()
 @auth
 def get_quote():
     quote = QuoteView()
